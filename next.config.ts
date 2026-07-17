51f0acdf85c6160b13fc30867337fc4d1f780b42
Nohaila Lourak <nlourak@smb.sqli.com>
2026-07-17 08:27:01 +0100

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/demo-cloudrun/:tag/:path*', 
+        destination: '/:path*',
+      },
+      {
+        source: '/demo-cloudrun/:tag',
+        destination: '/',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
